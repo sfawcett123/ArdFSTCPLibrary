7cbded3ba3cb01825ab026d0a88b1303a3a50f9f
Steve Fawcett <stevenfawcett38@gmail.com>
2022-11-07 21:09:27 +0000

    Seperated from Flight Simulator Project

---
+++ FlightSimulator.cpp
@@ -0,0 +1,68 @@
+#include "FlightSimulator.h"
+
+String contentType = "application/json";
+String postData ;
+String url = "/api/simulator/register";
+
+char *boardName ;
+
+StaticJsonDocument<100> json_data;
+StaticJsonDocument<100> response_data;
+
+Client* xWifiClient;
+
+FlightSimulator::FlightSimulator(Client& aClient, const char* aServerName, uint16_t aServerPort , const char* aName )
+   : HttpClient(aClient, aServerName , aServerPort)
+{
+  xWifiClient = &aClient;
+  boardName =  aName ;
+}
+
+void FlightSimulator::BuildPostData( String outputs[] )
+{
+  json_data["name"]       =  boardName;
+  json_data["os_system"]  = "Arduino";
+  json_data["os_version"] =  VERSION;
+
+  JsonArray data = json_data.createNestedArray("outputs");
+
+  for (int i = 0; i < ARRAY_SIZE(outputs); i++) {
+    data.add( outputs[i] ) ;
+  }
+  serializeJson( json_data , postData );
+}
+
+int FlightSimulator::Register( String outputs[] )
+{
+  BuildPostData( outputs );
+
+  post( url, contentType, postData);
+
+  int statusCode = responseStatusCode();
+  String response = responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+
+  DeserializationError error = deserializeJson(response_data, response);
+
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  char* port = response_data["port"];
+
+  return String( port ).toInt();
+}
+
+void FlightSimulator::ReadFSData( DynamicJsonDocument *SimulatorData)
+{
+  DeserializationError error = deserializeJson( *SimulatorData, *xWifiClient);
+ 
+}
+
+
---
+++ FlightSimulator.h
@@ -0,0 +1,26 @@
+#ifndef FLIGHTSIMULATOR_H
+	#define FLIGHTSIMULATOR_H
+
+  #define VERSION "0.0.0"
+	#include <Arduino.h>
+  #include <ArduinoHttpClient.h>  /* https://github.com/arduino-libraries/ArduinoHttpClient */
+  #include <WiFiNINA.h>
+  #include <ArduinoJson.h>        /* https://arduinojson.org/?utm_source=meta&utm_medium=library.properties */
+
+  #define ARRAY_SIZE(array) ((sizeof(array))/(sizeof(array[0])))
+
+    class FlightSimulator : public HttpClient
+    {
+        public:
+            static const int kHttpPort = 80;
+            
+
+            FlightSimulator(Client& aClient, const char* aServerName, uint16_t aServerPort = kHttpPort , const char* aName = "Arduino");
+            int Register( String outputs[] );
+            void ReadFSData( DynamicJsonDocument *SimulatorData );
+        
+        private:
+            void FlightSimulator::BuildPostData(String outputs[]);
+    };
+
+#endif
